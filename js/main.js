299a5afb145294bd5503436f435abca59d028e45
johlmike <johlmike@gmail.com>
2016-05-11 01:34:38 +0800

    修正Modal顯示
    讓密室Modal可以點擊按鈕開啟預約Modal，不會出現Scroll Bar問題

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
 /**
  * Created by Johlmike on 2016/5/4.
  */
-document.getElementById('up-img').addEventListener("click", upBG, false);
-document.getElementById('down-img').addEventListener("click", downBG, false);
+// document.getElementById('up-img').addEventListener("click", upBG, false);
+// document.getElementById('down-img').addEventListener("click", downBG, false);
+//
+// var headImgNum = 1;
+// var imgName = '';
+// var headImgElement = document.getElementById('head-img-div');
+//
+//
+// function upBG () {
+//     headImgNum -= 1;
+//     if( headImgNum ===  0){
+//         headImgNum = 8;
+//     }
+//     imgName = 'opposite_room_' + headImgNum + '.jpg';
+//     headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
+// }
+//
+// function downBG () {
+//     headImgNum += 1;
+//     if( headImgNum ===  9){
+//         headImgNum = 1;
+//     }
+//     imgName = 'opposite_room_' + headImgNum + '.jpg';
+//     headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
+// }
 
-var headImgNum = 1;
-var imgName = '';
-var headImgElement = document.getElementById('head-img-div');
+$('.btn-reser-modal').click(function(e) {
+    e.preventDefault();
 
+    $('.modal-room')
+        .modal('hide')
+        .on('hidden.bs.modal', function(e) {
+            $('#reservate').modal('show');
 
-function upBG () {
-    headImgNum -= 1;
-    if( headImgNum ===  0){
-        headImgNum = 8;
-    }
-    imgName = 'opposite_room_' + headImgNum + '.jpg';
-    headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
-}
+            $(this).off('hidden.bs.modal'); // Remove the 'on' event binding
+        });
 
-function downBG () {
-    headImgNum += 1;
-    if( headImgNum ===  9){
-        headImgNum = 1;
-    }
-    imgName = 'opposite_room_' + headImgNum + '.jpg';
-    headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
-}
+});
