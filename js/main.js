d73eb0b6bc5cabc378fcec9e538c307f03faec5f
johlmike <johlmike@gmail.com>
2016-05-12 01:19:53 +0800

    delete unnecessary codes

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-/**
- * Created by Johlmike on 2016/5/4.
- */
-// document.getElementById('up-img').addEventListener("click", upBG, false);
-// document.getElementById('down-img').addEventListener("click", downBG, false);
-//
-// var headImgNum = 1;
-// var imgName = '';
-// var headImgElement = document.getElementById('head-img-div');
-//
-//
-// function upBG () {
-//     headImgNum -= 1;
-//     if( headImgNum ===  0){
-//         headImgNum = 8;
-//     }
-//     imgName = 'opposite_room_' + headImgNum + '.jpg';
-//     headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
-// }
-//
-// function downBG () {
-//     headImgNum += 1;
-//     if( headImgNum ===  9){
-//         headImgNum = 1;
-//     }
-//     imgName = 'opposite_room_' + headImgNum + '.jpg';
-//     headImgElement.style.backgroundImage = 'url("img/' + imgName + '")';
-// }
-
 $('.btn-reser-modal').click(function(e) {
     e.preventDefault();
 
