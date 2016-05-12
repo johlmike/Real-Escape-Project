b8222f6629c26ba7d33bfa998765ae64775e13d5
johlmike <johlmike@gmail.com>
2016-05-12 10:20:11 +0800

    修正對應的房間modal開啟預約modal
    讓房間Modal開啟預約時會選擇對應的房間名稱
    移除about.html

--- js/main.js
+++ js/main.js
@@ -4,9 +4,37 @@ $('.btn-reser-modal').click(function(e) {
     $('.modal-room')
         .modal('hide')
         .on('hidden.bs.modal', function(e) {
+            var $roomModal = $('#reser-room').children();
+            var id = $(this).attr('id');
             $('#reservate').modal('show');
+
+            switch (id) {
+                case 'opposite':
+                    $('#reser-room option[value="opposite-room"]').prop('selected', 'true');
+                    break;
+                case 'strange-case':
+                    $('#reser-room option[value="strange-room"]').prop('selected', 'true');
+                    break;
+                case 'chain':
+                    $('#reser-room option[value="chain-room"]').prop('selected', 'true');
+                    break;
+                case 'cage':
+                    $('#reser-room option[value="cage-room"]').prop('selected', 'true');
+                    break;
+                case 'village':
+                    $('#reser-room option[value="village-room"]').prop('selected', 'true');
+                    break;
+                case 'reset':
+                    $('#reser-room option[value="reset-room"]').prop('selected', 'true');
+                    break;
+            }
 
             $(this).off('hidden.bs.modal'); // Remove the 'on' event binding
         });
 
+});
+
+
+$('#btn-jumbotron').click(function() {
+    $('#reser-room option[value="opposite-room"]').prop('selected', 'true');
 });
